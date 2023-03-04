19607b58191bc2ca950276fe52044d8f8f5340c5
Tirth Govani <govanitirth@gmail.com>
2023-03-04 11:39:00 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import ReactMarkdown from 'react-markdown'
+import Button from '@mui/material/Button';
+import Snackbar from '@mui/material/Snackbar';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import Alert from '@mui/material/Alert';
+
 
 function App() {
+  const [markdown, setMarkdown] = useState('# markdown preview')
+  const [open, setOpen] =   useState(false);
+
+  const handleClick = (e) => {
+    navigator.clipboard.writeText(e)
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Button style={{ margin: "10px 65px" }} className="btn" onClick={() => handleClick(markdown)}>Copy Text
+        <ContentCopyIcon />
+      </Button>
+      <Snackbar open={open} autoHideDuration={2000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+          copied to Clipboard!!
+        </Alert>
+      </Snackbar>
+      <section className='markdown'>
+        <textarea className='input'
+          value={markdown}
+          onChange={(e) => setMarkdown(e.target.value)}
+        />
+        <article className='result'>
+          <ReactMarkdown>{markdown}</ReactMarkdown>
+        </article>
+      </section>
+    </>
   );
 }
 
